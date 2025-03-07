fc99ec6c06f061e25bd440cb5a5dc2dcefe113b0
mike <mick@mickvc.com>
2025-03-07 22:37:23 +0700

    fix: move tailwind config into a separate package (root)

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,7 +6,7 @@ const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:5000";
 export default function App() {
   return (
     <div className="w-full min-h-screen bg-black text-white flex flex-col justify-center items-center">
-      <main className="flex flex-col justify-center items-center gap-2 w-full max-w-lg">
+      <main className="flex flex-col justify-center items-center gap-4 w-full max-w-lg">
         <div className="">heimursaga</div>
         <Button>ok</Button>
       </main>
