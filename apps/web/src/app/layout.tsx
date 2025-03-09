e31450439035828db9b687a2e4e5eac73eb22c18
mike <mick@mickvc.com>
2025-03-09 13:09:01 +0700

    feat: add metadata and favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,10 +1,23 @@
 import '@repo/ui/globals.css';
+import { Metadata } from 'next';
 
 import { AppFooter, AppHeader } from '@/components';
+
+export const metadata: Metadata = {
+  title: 'saga',
+  description: 'a journaling platform for travelers',
+  openGraph: {
+    title: 'saga',
+    description: 'a journaling platform for travelers',
+  },
+};
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
       <body>
         <div className="w-full min-h-screen bg-neutral-100 text-black flex flex-col justify-start">
           <AppHeader />
