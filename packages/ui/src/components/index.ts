254fc814360177aa980a6719b59f225b50f4b011
mike <mick@mickvc.com>
2025-03-09 11:30:44 +0700

    feat: shadcn alert, badge, dialog, form, input, label, select, switch, textarea.

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,2 +1,12 @@
-export * from "./button";
-export * from "./logo";
+export * from './button';
+export * from './alert';
+export * from './badge';
+export * from './dialog';
+export * from './form';
+export * from './input';
+export * from './label';
+export * from './select';
+export * from './switch';
+export * from './textarea';
+
+export * from './logo';
