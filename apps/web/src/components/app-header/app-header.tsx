a8c5c84568a977054e4dc194c5ab526372822c1a
mike <mick@mickvc.com>
2025-03-09 10:55:58 +0700

    feat: prettier

--- apps/web/src/components/app-header/app-header.tsx
+++ apps/web/src/components/app-header/app-header.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
+import { Button, Logo } from '@repo/ui/components';
+import Link from 'next/link';
 
-import { Button, Logo } from "@repo/ui/components";
-import { ROUTER } from "@/router";
+import { ROUTER } from '@/router';
 
 export const AppHeader = () => {
   return (
