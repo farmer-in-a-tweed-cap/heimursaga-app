3e50bdde779a2fa2470eb9df0d6c1293a4d39393
mike <mick@mickvc.com>
2025-03-10 20:38:13 +0700

    feat: navigation links in the header [web]

--- apps/web/src/components/app-header/app-header.tsx
+++ apps/web/src/components/app-header/app-header.tsx
@@ -5,6 +5,13 @@ import Link from 'next/link';
 import { APP_HEADER_HEIGHT } from '@/constants';
 import { ROUTER } from '@/router';
 
+const data = {
+  menu: [
+    { href: ROUTER.EXPLORE, label: 'Explore' },
+    { href: ROUTER.HOME, label: 'Upgrade' },
+  ],
+};
+
 export const AppHeader = () => {
   return (
     <div
@@ -14,12 +21,27 @@ export const AppHeader = () => {
       )}
     >
       <div className="app-container max-w-full px-6 flex flex-row items-center justify-between">
-        <Link href={ROUTER.EXPLORE}>
+        <Link href={ROUTER.HOME}>
           <Logo />
         </Link>
-        <Link href={ROUTER.LOGIN}>
-          <Button>Log in</Button>
-        </Link>
+        <div className="flex flex-row items-center gap-14">
+          <nav>
+            <div className="flex flex-row items-center justify-start gap-6">
+              {data.menu.map(({ href, label }, key) => (
+                <Link
+                  key={key}
+                  href={href}
+                  className="capitalize text-sm font-medium"
+                >
+                  {label}
+                </Link>
+              ))}
+            </div>
+          </nav>
+          <Link href={ROUTER.LOGIN}>
+            <Button>Log in</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
