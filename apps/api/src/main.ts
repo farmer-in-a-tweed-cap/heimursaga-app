bb0fd2979cb6058e60e4c71b4d05e19cacfb1969
mike <mick@mickvc.com>
2025-03-10 09:33:06 +0700

    feat: prisma [api]

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './modules/app';
+import { AppModule } from '@/modules/app';
 
 const HOST = process.env.HOST || '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
-async function bootstrap() {
+async function main() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, HOST, () => {
-    console.log('api server running');
+    console.log('api running');
   });
 }
-bootstrap();
+
+main();
 
 process.on('uncaughtException', (e) => {
   console.log('uncaughtException', e);
