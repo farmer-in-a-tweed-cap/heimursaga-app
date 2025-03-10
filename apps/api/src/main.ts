799c6b96009b374c6ad7c3bd0980bce57059f00e
mike <mick@mickvc.com>
2025-03-10 10:16:34 +0700

    feat: fastify [api]

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,86 @@
 import { NestFactory } from '@nestjs/core';
+import { RequestMethod } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { fastifyCors, FastifyCorsOptions } from '@fastify/cors';
+import { fastifyCookie, FastifyCookieOptions } from '@fastify/cookie';
+import { fastifyMultipart, FastifyMultipartOptions } from '@fastify/multipart';
+import fastifyHelmet, { FastifyHelmetOptions } from '@fastify/helmet';
 
 import { AppModule } from '@/modules/app';
 
-const HOST = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || 5000;
-
 async function main() {
-  const app = await NestFactory.create(AppModule);
+  const NODE_ENV = process.env?.NODE_ENV || 'development';
+  const HOST = process.env?.HOST || '0.0.0.0';
+  const PORT = parseInt(process.env.PORT) || 5000;
+  const API_VERSION = 1;
+  const API_PREFIX = `v${API_VERSION}`;
+  const COOKIE_SECRET = process.env.COOKIE_SECRET;
+  const CORS_ORIGIN = 'localhost';
+  const IS_PRODUCTION = NODE_ENV === 'production';
+  const IS_DEVELOPMENT = NODE_ENV === 'development';
+
+  // create a fastify adapter
+  const adapter = new FastifyAdapter({
+    trustProxy: true,
+    bodyLimit: 2 * 1024 * 1024,
+  });
+
+  // create a fastify instance
+  const fastify = adapter.getInstance();
+
+  // set fastify plugins
+  await fastify.register<FastifyCorsOptions>(fastifyCors as any, {
+    origin: CORS_ORIGIN,
+    credentials: true,
+    methods: 'GET,POST,PATCH,PUT,DELETE',
+  } satisfies FastifyCorsOptions);
+
+  await fastify.register<FastifyCookieOptions>(fastifyCookie as any, {
+    secret: COOKIE_SECRET,
+    parseOptions: {
+      httpOnly: true,
+      path: '/',
+    },
+  } satisfies FastifyCookieOptions);
+
+  await fastify.register<FastifyMultipartOptions>(
+    fastifyMultipart as any,
+    {} satisfies FastifyMultipartOptions,
+  );
+
+  await fastify.register<FastifyHelmetOptions>(fastifyHelmet as any);
+
+  // create an app instance
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+    { rawBody: true, snapshot: true },
+  );
+
+  // set a global prefix (e.g. /v1/*)
+  app.setGlobalPrefix(API_PREFIX, {
+    exclude: [
+      {
+        path: '',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
+
+  // run the app
   await app.listen(PORT, HOST, () => {
     console.log('api running');
   });
 }
 
+// build the app
 main();
 
+// handle exceptions
 process.on('uncaughtException', (e) => {
   console.log('uncaughtException', e);
 });
---
+++ apps/api/src/types/environment.d.ts
@@ -0,0 +1,27 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'staging';
+      HOST: string;
+      PORT: string;
+      TZ: string;
+
+      APP_DOMAIN: string;
+      CORS_ORIGIN: string;
+      DATABASE_URL: string;
+
+      SESSION_SECRET: string;
+      COOKIE_SECRET: string;
+
+      STRIPE_SECRET_KEY: string;
+      STRIPE_WEBHOOK_SECRET: string;
+
+      SMTP_HOST: string;
+      SMTP_PORT: string;
+      SMTP_USER: string;
+      SMTP_PASSWORD: string;
+    }
+  }
+}
+
+export {};
