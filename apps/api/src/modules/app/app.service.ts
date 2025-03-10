612734d4c7a15150a7e1a9618382d341e3ecf846
mike <mick@mickvc.com>
2025-03-10 13:09:47 +0700

    feat: test endpoint [api]

--- apps/api/src/modules/app/app.service.ts
+++ apps/api/src/modules/app/app.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '@/modules/prisma';
+
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async test() {
     try {
       const results = {
-        users: 0,
-        posts: 0,
+        users: await this.prisma.user.count(),
+        posts: await this.prisma.post.count(),
       };
 
       return results;
