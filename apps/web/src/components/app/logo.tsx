09dc5746ff3a73b34d34ea85f0c72c5c075787b7
mike <mick@mickvc.com>
2025-05-21 13:14:14 +0700

    fix: logo size [web]

--- apps/web/src/components/app/logo.tsx
+++ apps/web/src/components/app/logo.tsx
@@ -27,25 +27,25 @@ export const Logo: React.FC<Props> = ({ color = 'dark', size = 'sm' }) => {
 };
 
 const LogoLgLight = () => (
-  <div className="w-[140px] h-auto bg-red-500">
+  <div className="w-[140px] h-[50px] flex flex-row items-center justify-center">
     <Image src="/logo-lg-light.svg" width={140} height={80} alt="logo" />
   </div>
 );
 
 const LogoLgDark = () => (
-  <div className="w-[140px] h-auto bg-red-500">
+  <div className="w-[140px] h-[50px] flex flex-row items-center justify-center">
     <Image src="/logo-lg-dark.svg" width={140} height={80} alt="logo" />
   </div>
 );
 
 const LogoSmLight = () => (
-  <div className="w-[45px] h-auto">
+  <div className="w-[45px] h-[45px]">
     <Image src="/logo-sm-light.svg" width={80} height={80} alt="" />
   </div>
 );
 
 const LogoSmDark = () => (
-  <div className="w-[45px] h-auto">
+  <div className="w-[45px] h-[45px]">
     <Image src="/logo-sm-dark.svg" width={80} height={80} alt="" />
   </div>
 );
