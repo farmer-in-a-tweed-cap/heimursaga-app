f51d00140e7e7cc5e84404cb868bd3681c8bbca3
mike <mick@mickvc.com>
2025-05-21 12:06:58 +0700

    fix: return empty sitemap as fallback [web]

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -3,23 +3,28 @@ import type { MetadataRoute } from 'next';
 import { apiClient } from '@/lib/api';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const sitemap = await apiClient.generateSitemap();
+  try {
+    const sitemap = await apiClient.generateSitemap();
 
-  const sources = sitemap.data?.sources || [];
+    const sources = sitemap.data?.sources || [];
 
-  return sources.map(
-    ({ loc, lastmod, priority = 1, changefreq = 'weekly' }) => ({
-      url: loc,
-      lastModified: lastmod,
-      changeFrequency: changefreq as
-        | 'always'
-        | 'hourly'
-        | 'daily'
-        | 'weekly'
-        | 'monthly'
-        | 'yearly'
-        | 'never',
-      priority,
-    }),
-  );
+    return sources.map(
+      ({ loc, lastmod, priority = 1, changefreq = 'weekly' }) => ({
+        url: loc,
+        lastModified: lastmod,
+        changeFrequency: changefreq as
+          | 'always'
+          | 'hourly'
+          | 'daily'
+          | 'weekly'
+          | 'monthly'
+          | 'yearly'
+          | 'never',
+        priority,
+      }),
+    );
+  } catch (e) {
+    // return an empty sitemap as fallback
+    return [];
+  }
 }
