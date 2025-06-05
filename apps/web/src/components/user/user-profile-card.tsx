2271c6be28bc3e2de85ea4b99c6cb36e6fea0ac9
mike <mick@mickvc.com>
2025-06-05 13:29:57 +0700

    fix: user profile card styles [web]

--- apps/web/src/components/user/user-profile-card.tsx
+++ apps/web/src/components/user/user-profile-card.tsx
@@ -25,45 +25,44 @@ export const UserProfileCard: React.FC<Props> = ({
 }) => {
   return (
     <Card>
-      <CardContent className="pt-2 pb-4">
-        <div className="absolute left-2 top-2">
-          <BackButton href={backButton?.href} onClick={backButton?.click} />
-        </div>
-        <div className="flex flex-col items-center justify-center gap-0">
-          <UserAvatar
-            className="w-[40px] h-[40px]"
-            src={picture}
-            loading={loading}
-            fallback={name}
-          />
-
-          <div className="mt-1 flex flex-col items-center gap-0">
-            {loading ? (
-              <div className="h-6 flex items-center justify-center">
-                <Skeleton className="w-[100px] h-4" />
-              </div>
-            ) : (
-              <span className="h-6 font-medium text-base">{name}</span>
-            )}
-            {loading ? (
-              <Skeleton className="w-[80px] h-2" />
-            ) : (
-              <span className="text-xs font-medium text-gray-600">
-                @{username}
-              </span>
-            )}
+      <CardContent className="p-4 flex flex-row items-center justify-between">
+        <div className="flex flex-row gap-2">
+          <div className="">
+            <BackButton href={backButton?.href} onClick={backButton?.click} />
           </div>
-          <div className="mt-4">
-            <Button variant="secondary" size="sm" asChild>
-              <Link
-                href={username ? ROUTER.USERS.DETAIL(username) : '#'}
-                className="flex flex-row items-center justify-center gap-1"
-              >
-                Profile
-              </Link>
-            </Button>
+          <div className="flex flex-row items-center justify-start gap-2">
+            <UserAvatar
+              className="w-[40px] h-[40px]"
+              src={picture}
+              loading={loading}
+              fallback={name}
+            />
+            <div className="mt-1 flex flex-col items-start justify-start  gap-0">
+              {loading ? (
+                <div className="h-6 flex items-center justify-center">
+                  <Skeleton className="w-[100px] h-4" />
+                </div>
+              ) : (
+                <span className="h-6 font-medium text-base">{name}</span>
+              )}
+              {loading ? (
+                <Skeleton className="w-[80px] h-2" />
+              ) : (
+                <span className="text-xs font-medium text-gray-600">
+                  @{username}
+                </span>
+              )}
+            </div>
           </div>
         </div>
+        <Button variant="secondary" size="sm" asChild>
+          <Link
+            href={username ? ROUTER.USERS.DETAIL(username) : '#'}
+            className="flex flex-row items-center justify-center gap-1"
+          >
+            Profile
+          </Link>
+        </Button>
       </CardContent>
     </Card>
   );
