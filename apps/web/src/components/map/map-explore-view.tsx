8f7a0c0d8e74e59f8f6619d2f9a3d709c1e267e0
mike <mick@mickvc.com>
2025-06-12 21:11:30 +0700

    fix: post card button margin [web]

--- apps/web/src/components/map/map-explore-view.tsx
+++ apps/web/src/components/map/map-explore-view.tsx
@@ -433,6 +433,7 @@ export const MapExploreView: React.FC<Props> = () => {
     updateParams({
       context: MapQueryContext.GLOBAL,
       user: null,
+      filter: null,
     });
 
     mapQuery.refetch();
