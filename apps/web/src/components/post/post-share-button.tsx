618ff1289c1b4a5bd222ec81acff296a1c0798c8
mike <mick@mickvc.com>
2025-06-12 14:03:51 +0700

    feat: post share button [web]

---
+++ apps/web/src/components/post/post-share-button.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Button } from '@repo/ui/components';
+import { useToast } from '@repo/ui/hooks';
+import { ShareFatIcon, ShareIcon } from '@repo/ui/icons';
+import { cn } from '@repo/ui/lib/utils';
+
+import { ROUTER } from '@/router';
+
+type Props = {
+  postId?: string;
+  onClick?: () => void;
+};
+
+export const PostShareButton: React.FC<Props> = ({ postId }) => {
+  const toast = useToast();
+
+  const handleClick = () => {
+    if (!postId) return;
+
+    const url = new URL(
+      ROUTER.POSTS.DETAIL(postId),
+      process.env.NEXT_PUBLIC_APP_BASE_URL,
+    ).toString();
+    navigator.clipboard.writeText(url);
+    toast({ type: 'message', message: 'the post link copied' });
+  };
+
+  return (
+    <Button
+      variant="ghost"
+      className="transition-all hover:bg-accent rounded-full h-[32px]"
+      size="sm"
+      title="Share"
+      onClick={handleClick}
+    >
+      <ShareFatIcon size={24} weight="bold" />
+    </Button>
+  );
+};
