663467751c2b6daf5a0f282714c8837865698e6f
mike <mick@mickvc.com>
2025-06-26 12:13:05 +0700

    fix: post card userbar link and like button active [web]

--- apps/web/src/components/post/post-card.tsx
+++ apps/web/src/components/post/post-card.tsx
@@ -109,7 +109,6 @@ export const PostCard: React.FC<PostCardProps> = ({
         ) : (
           <></>
         )}
-
         <div className="relative flex flex-row justify-between items-center">
           <div className="w-auto flex flex-row justify-start items-center gap-3 z-20">
             {userbar?.href ? (
@@ -221,21 +220,19 @@ export const PostCard: React.FC<PostCardProps> = ({
             </div>
           )}
         </div>
-
         {(actions.like || actions.share) && (
-          <div className="mt-6">
-            <PostButtons
-              postId={id}
-              actions={{
-                ...actions,
-                bookmark: false,
-              }}
-              liked={liked}
-              likesCount={likesCount}
-              bookmarked={bookmarked}
-              bookmarksCount={bookmarksCount}
-            />
-          </div>
+          <PostButtons
+            className="relative mt-6 z-20"
+            postId={id}
+            actions={{
+              ...actions,
+              bookmark: false,
+            }}
+            liked={liked}
+            likesCount={likesCount}
+            bookmarked={bookmarked}
+            bookmarksCount={bookmarksCount}
+          />
         )}
       </CardContent>
     </Card>
